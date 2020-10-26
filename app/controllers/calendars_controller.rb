1d60ac7d917ee300bbd739633526f70227fc6b46
hitochan151 <fbx3jpef6@gmail.com>
2020-10-26 20:16:59 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,9 +34,23 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = Date.today.wday + x
+      #もしもwday_numが7以上であれば、7を引く
+      if 
+        wday_num = wday_num - 7
+      end
+
+      days = { 
+        month: (@todays_date + x).month, 
+        date: (@todays_date+x).day, 
+        wday:wdays[wday_num],
+        plans: today_plans
+      }
       @week_days.push(days)
     end
-
   end
 end
+# Date.today.wday 今日の曜日を数値化したもの
+# timesメソッドを活用して繰り返すたびに＋１したい
+# 配列名[添字]
